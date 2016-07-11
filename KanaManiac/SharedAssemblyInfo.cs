d04c9d1603594d0c6c16748bcb171178c0b3c925
Artyom Fomin <mail@artzilla.name>
2016-07-11 21:11:51 +0600

    configurating environment

---
+++ KanaManiac/SharedAssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+ 
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KanaManiac")]
+[assembly: AssemblyDescription("This is KanaManiac assembly for learning japanese")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("KanaManiac")]
+[assembly: AssemblyCopyright("Copyright © ArtZilla's development 2016")]
+[assembly: AssemblyCompany("ArtZilla's development")]
+[assembly: AssemblyVersion("0.0.1.0")]
